19d960eb1fa4aac07582c6da35f71dfd22d972e3
jeroen.vanlier <jeroen.vanlier@flash.co.za>
2024-01-25 23:56:16 +0200

    final touches

---
+++ live_api_calls.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+data_0 = {
+    "age": 18,
+    "workclass": "Local_gov",
+    "fnlgt": 1.794650e05,
+    "education": "10th",
+    "education_num": 8,
+    "marital_status": "Never_married",
+    "occupation": "Farming_fishing",
+    "relationship": "Not_in_family",
+    "race": "Black",
+    "sex": "Female",
+    "capital_gain": 100,
+    "capital_loss": 50,
+    "hours_per_week": 0,
+    "native_country": "Mexico",
+}
+
+data_1 = {
+    "age": 50,
+    "workclass": "Private",
+    "fnlgt": 1.880050e05,
+    "education": "Masters",
+    "education_num": 12,
+    "marital_status": "Married_civ_spouse",
+    "occupation": "Exec_managerial",
+    "relationship": "Husband",
+    "race": "White",
+    "sex": "Male",
+    "capital_gain": 40174,
+    "capital_loss": 195,
+    "hours_per_week": 50,
+    "native_country": "United_States",
+}
+
+headers = {'Content-Type': 'application/json'}
+
+url = 'https://cicd-api-cloud.onrender.com/predict' 
+
+response = requests.post(url, data=json.dumps(data_0), headers=headers)
+print(response.text)
+
+response = requests.post(url, data=json.dumps(data_1), headers=headers)
+print(response.text)
