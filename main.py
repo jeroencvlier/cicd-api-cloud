a2b84a9f8c569e449debaf355b0234ff265dea7b
jeroen.vanlier <jeroen.vanlier@flash.co.za>
2024-01-25 23:08:26 +0200

    black formatting

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ from src.ml.training import infer_ml, load_model
 
 
 app = FastAPI(
-    title='Income Prediction API',
-    description='This API uses a trained model to predict the income of a person. Returns 1 if the income is greater than 50K, and 0 otherwise.',
-    version='0.1',
+    title="Income Prediction API",
+    description="This API uses a trained model to predict the income of a person. Returns 1 if the income is greater than 50K, and 0 otherwise.",
+    version="0.1",
 )
 
 
@@ -43,14 +43,14 @@ class IncomePrediction(BaseModel):
 def catagorical_check():
     pipe, _, _ = load_model()
 
-    preprocessor = pipe.named_steps['preprocessor']
+    preprocessor = pipe.named_steps["preprocessor"]
     transformers = preprocessor.transformers_
 
     for transformer_name, _, columns in transformers:
-        if transformer_name == 'cat':
+        if transformer_name == "cat":
             cat_columns = columns
-            cat_pipeline = preprocessor.named_transformers_['cat']
-            onehotencoder = cat_pipeline.named_steps['onehotencoder']
+            cat_pipeline = preprocessor.named_transformers_["cat"]
+            onehotencoder = cat_pipeline.named_steps["onehotencoder"]
             categories = onehotencoder.categories_
 
     return cat_columns, categories
@@ -66,16 +66,24 @@ async def exercise_function(data: IncomePrediction):
     cat_columns, categories = catagorical_check()
     for col, catagory in zip(cat_columns, categories):
         if getattr(data, col) not in catagory:
-            raise HTTPException(
-                status_code=400, detail=f"Invalid input for {col}")
+            raise HTTPException(status_code=400, detail=f"Invalid input for {col}")
 
-    if getattr(data, "age") < 0 or getattr(data, "fnlgt") < 0 or getattr(data, "education_num") < 0 or getattr(data, "capital_gain") < 0 or getattr(data, "capital_loss") < 0 or getattr(data, "hours_per_week") < 0:
+    if (
+        getattr(data, "age") < 0
+        or getattr(data, "fnlgt") < 0
+        or getattr(data, "education_num") < 0
+        or getattr(data, "capital_gain") < 0
+        or getattr(data, "capital_loss") < 0
+        or getattr(data, "hours_per_week") < 0
+    ):
         raise HTTPException(
-            status_code=400, detail=f"Invalid input for numerical columns")
+            status_code=400, detail=f"Invalid input for numerical columns"
+        )
 
     if getattr(data, "age") > 110 or getattr(data, "hours_per_week") > 100:
         raise HTTPException(
-            status_code=400, detail=f"Invalid input for numerical columns")
+            status_code=400, detail=f"Invalid input for numerical columns"
+        )
 
     data = pd.DataFrame([data.dict()])
     pred, pred_label = infer_ml(data)
--- src/train_model.py
+++ src/train_model.py
@@ -2,6 +2,6 @@ from data.ingest_data import clean_main
 from ml.training import train_ml
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     clean_main()
     train_ml()
